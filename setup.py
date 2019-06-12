b39620df11b56f5bfbfe4c513fbb0fdd9537a086
eatmanCTF <eatman@mail.com>
2019-06-12 14:37:19 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="roputils",
+    version="0.1",
+    description="rop utils",
+    author="eatman",
+    author_email="eatman@mail.com",
+    platforms=["linux"],
+    license="BSD",
+    url="https://github.com/eatmanCTF/roputils",
+    packages=find_packages())
